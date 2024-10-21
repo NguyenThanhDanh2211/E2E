f8f0e7264da36e5f05e47ffbf2950926f1724c76
Nguyen Thanh Danh <nguyenthanhdanh221102@gmail.com>
2024-10-21 21:24:19 +0700

    E2E Test using GitHub Actions+Parallel Execution using GitHub Actions

---
+++ cypress/e2e/tests/addToCartTest.cy.js
@@ -0,0 +1,18 @@
+import { homePage } from '../../pages/homePage';
+import testData from '../../fixtures/testData.json';
+
+const homePageObj = new homePage();
+
+describe('test automation', () => {
+  before(() => {
+    cy.login(testData.login.username, testData.login.password);
+  });
+
+  it('Add To Cart flow', () => {
+    homePageObj.searchProduct(testData.product.productName);
+    homePageObj.addToCart();
+    homePageObj
+      .verifySuccessMessage()
+      .should('contain', testData.message.successMessage);
+  });
+});
